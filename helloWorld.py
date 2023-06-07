99a2e309d0c7e71e9bf8eee82dd80477a003eb03
Seong1-Jo <seong12yada@gmail.com>
2023-06-07 19:24:00 +0900

    feat: Print numbers for any other cases

--- helloWorld.py
+++ helloWorld.py
@@ -1,8 +1,12 @@
 const a = 3
 for(let i = 1; i < 10; i++){
     if(a === 3) print('hello')
-    else {
+    else if {
         print('bye')
         }
+    else {
+        print('by2');
+        }
+    
 }
 
